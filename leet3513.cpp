3ae1162bdf08ddd4621c560da5952b88f3c24e6a
Vishal Kashyap <vishalkashyapvk200@gmail.com>
2026-07-23 22:42:49 +0530

    LeetCode Update - 2026-07-23 22:42:49

---
+++ leet3513.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return n;
+
+        // Find the number of bits in 'n'
+        int bits = 0;
+        int temp = n;
+        while (temp > 0) {
+            bits++;
+            temp >>= 1;
+        }
+
+        return 1 << bits;
+    }
+};
+
+int main() {
+    // 1. Create an instance of the Solution class
+    Solution solution;
+    
+    // 2. Define a test case 
+    vector<int> nums = {1, 2, 3}; 
+    
+    // 3. Call the function and print the result
+    int result = solution.uniqueXorTriplets(nums);
+    cout << "Output: " << result << endl;
+
+    return 0;
+}
